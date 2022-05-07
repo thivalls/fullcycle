d5ed50171fe94036782121101663dca765254162
valls <olavalls@gmail.com>
2022-05-07 02:25:48 -0300

    Applying DDD on category domain

---
+++ domain/src/main/java/com/fullcycle/admin/catalog/domain/AggregateRoot.java
@@ -0,0 +1,9 @@
+package com.fullcycle.admin.catalog.domain;
+
+public abstract class AggregateRoot<ID extends Identifier> extends Entity<ID> {
+
+    protected AggregateRoot(ID id) {
+        super(id);
+    }
+
+}
