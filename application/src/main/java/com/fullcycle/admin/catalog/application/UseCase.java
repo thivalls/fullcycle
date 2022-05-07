11486cdcc8b50e06d388bf6f6ba4ed5c4a9d40d7
valls <olavalls@gmail.com>
2022-05-07 02:09:05 -0300

    creating constructor method for category domain

--- application/src/main/java/com/fullcycle/admin/catalog/application/UseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalog/application/UseCase.java
@@ -1,9 +1,9 @@
 package com.fullcycle.admin.catalog.application;
 
-import com.fullcycle.admin.catalog.domain.Category;
+import com.fullcycle.admin.catalog.domain.category.Category;
 
 public class UseCase {
     public Category execute() {
-        return new Category();
+        return Category.newCategory("dfdfidfidf", "some description", true);
     }
 }
--- domain/src/test/java/com/fullcycle/admin/catalog/domain/CategoryTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalog/domain/CategoryTest.java
@@ -1,15 +1,33 @@
 package com.fullcycle.admin.catalog.domain;
 
+import com.fullcycle.admin.catalog.domain.category.Category;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class CategoryTest {
 
-    @Test
-    void test1 () {
-        Assertions.assertNotNull(new CategoryTest());
-    }
+//    @Test
+//    @DisplayName("Should be possible create an instance of Category")
+//    void test1 () {
+//        Assertions.thr
+//    }
 
+    @Test
+    @DisplayName("Should be possible to create a new category with correct params")
+    void test2 () {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        Category category = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertNotNull(category);
+        Assertions.assertEquals(expectedName, category.getName());
+        Assertions.assertEquals(expectedDescription, category.getDescription());
+        Assertions.assertEquals(expectedIsActive, category.getActive());
+        Assertions.assertNotNull(category.getCreatedAt());
+        Assertions.assertNotNull(category.getUpdatedAt());
+        Assertions.assertNull(category.getDeletedAt());
+    }
 }
