87ec8642e0cc83cf9636261f8c1f834d5112d132
valls <olavalls@gmail.com>
2022-10-09 19:52:38 -0300

    Adding springframework to infrastructure and configuring environment property file

--- infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Main.java
@@ -1,7 +1,15 @@
 package com.fullcycle.admin.catalog.infrastructure;
 
+import com.fullcycle.admin.catalog.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "development"); para setar profile active
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.fullcycle.admin.catalog.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fullcycle.admin.catalog")
+public class WebServerConfig {
+}
